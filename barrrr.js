b37ef54197d3ce25bb2b248974c5ae4a5375f6f2
Nifesi Ajikiola <153874112+zeemorayo@users.noreply.github.com>
2024-06-14 22:40:36 +0100

    Add files via upload

---
+++ barrrr.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  titleWeb: "Bardi",
+
+  background: "background.jpeg",
+
+  min1: "pinkie.jpeg",
+  min2: "red.jpeg",
+  min3: "skims.jpeg",
+  min4: "smileywhite.jpeg",
+  min5: "iop.jpeg",
+  min6: "gold.jpeg",
+
+  max1: "whitebrows.jpeg",
+  max2: "vma.jpeg",
+  max3: "mama.jpeg",
+  max4: "redgala.jpeg",
+  max5: "cute.jpeg",
+  max6: "enough.jpeg",
+}
